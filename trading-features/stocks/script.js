52b6c85d19b22fab356606d58e436509105c3365
snaker938 <63957069+snaker938@users.noreply.github.com>
2021-08-28 23:22:37 +0100

    deleted files

--- trading-features/stocks/script.js
+++
@@ -1,6 +0,0 @@
-// Home Button :)
-const home = document.querySelector('.home');
-
-home.addEventListener('click', function () {
-  window.location.href = window.origin + '/' + 'trading-features/index.html';
-});
