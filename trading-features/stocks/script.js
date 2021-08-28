8a4b28ca4fc145d21fae945ef92489e3bdfb0482
snaker938 <63957069+snaker938@users.noreply.github.com>
2021-08-28 23:18:49 +0100

    Added files
    Added all files for the website to work

---
+++ trading-features/stocks/script.js
@@ -0,0 +1,6 @@
+// Home Button :)
+const home = document.querySelector('.home');
+
+home.addEventListener('click', function () {
+  window.location.href = window.origin + '/' + 'trading-features/index.html';
+});
